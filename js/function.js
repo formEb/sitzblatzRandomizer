53365d46f02f130ff898654e0a77008ee0da11c7
formEb <58007779+formEb@users.noreply.github.com>
2019-11-20 21:49:21 +0100

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,33 +1,8 @@
-var seets = document.getElementsByClassName("table");
+var htmlTableObjects = document.getElementsByClassName("table");
+var studentNames = ["Finn","Greta","Linus","Lukas","Arne","Dayana","Erik","Jens","Kevin","Lars","Leonie","Lolita","Lorena","Maalen","Milan","Ronja","Tewes","Vincent","Timo","Jan"];
 
-function main() {   
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', "http://formeb.github.io/sitzplanRandomizer/students.json", true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-      if (xobj.readyState == 4 && xobj.status == "200") {
-        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-        var yobj = new XMLHttpRequest();
-            yobj.overrideMimeType("application/json");
-            yobj.open('GET', "http://formeb.github.io/sitzplanRandomizer/assignment.json", true); // Replace 'my_data' with the path to your file
-            yobj.onreadystatechange = function () {
-                if (yobj.readyState == 4 && yobj.status == "200") {
-                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-                    var students = JSON.parse(xobj.responseText).students;
-                    var assignment = JSON.parse(yobj.responseText).seats;
-                    console.log(yobj)
-                    for (let i = 0; i<seets.length; i++) {
-                        seets[i].innerHTML = students[assignment[i]];
-                    }
-                }
-            };
-        yobj.send(null); 
-
-
-
-      }
-    };
-    xobj.send(null); 
- }
+for(let i = 0; i < studentNames.length; i++) {
+    htmlTableObjects[i].innerHTML = studentNames[seats[i]]
+}
 
  main();
