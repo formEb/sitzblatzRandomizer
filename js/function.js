f68bb919a9daa0e5d34a1766b5b28ad2eaab74d8
formEb <58007779+formEb@users.noreply.github.com>
2019-11-21 19:27:50 +0100

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,5 +1,5 @@
 var htmlTableObjects = document.getElementsByClassName("table");
-var studentNames = ["Finn","Greta","Linus","Lukas","Arne","Dayana","Erik","Jens","Kevin","Lars","Leonie","Lolita","Lorena","Maalen","Milan","Ronja","Tewes","Vincent","Timo","Jan"];
+var studentNames = ["Finn","Greta","Linus","Lukas","Arne","Dayana","Erik","Jens","Kevin","Lars","Leonie","Lolita","Lorena","Maalin","Milan","Ronja","Tewes","Vincent","Timo","Jan"];
 
 for(let i = 0; i < studentNames.length; i++) {
     htmlTableObjects[i].innerHTML = studentNames[seats[i]]
